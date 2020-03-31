cb6b6b93f996c74b94bb40274638b32abeb4b04b
andreistan26 <48967297+andreistan26@users.noreply.github.com>
2020-04-01 00:56:05 +0300

    Add files via upload

---
+++ JsonUtils.java
@@ -0,0 +1,65 @@
+package com.udacity.sandwichclub.utils;
+
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.udacity.sandwichclub.R;
+import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+
+        Sandwich sandwich = new Sandwich();
+        String mainName = "";
+        String description = "";
+        String placeOfOrigin = "";
+        String image = "";
+        List<String>lsAlsoKnownAs = new ArrayList<>();
+        List<String>lsIngredients = new ArrayList<>();
+
+
+        try{
+            JSONObject jobj = new JSONObject(json);
+
+            JSONObject objName = jobj.getJSONObject("name");
+            mainName = objName.getString("mainName");
+
+            JSONArray alsoKnownAs = objName.getJSONArray("alsoKnownAs");
+            for(int i=0 ; i<alsoKnownAs.length() ; i++){
+                lsAlsoKnownAs.add(alsoKnownAs.getString(i));
+            }
+
+            JSONArray ingredients = jobj.getJSONArray("ingredients");
+
+            for(int i=0 ; i<ingredients.length() ; i++){
+                lsIngredients.add(ingredients.getString(i));
+            }
+
+            description = jobj.getString("description");
+            image = jobj.getString("image");
+            placeOfOrigin = jobj.getString("placeOfOrigin");
+
+
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        sandwich.setAlsoKnownAs(lsAlsoKnownAs);
+        sandwich.setImage(image);
+        sandwich.setMainName(mainName);
+        sandwich.setIngredients(lsIngredients);
+        sandwich.setPlaceOfOrigin(placeOfOrigin);
+        sandwich.setDescription(description);
+
+        return sandwich;
+    }
+
+}
