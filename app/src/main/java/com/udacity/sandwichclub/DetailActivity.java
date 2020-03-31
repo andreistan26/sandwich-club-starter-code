f86e661ca1c6b3b3054759a4d3cbd7f2b82c7fe3
andreistan26 <48967297+andreistan26@users.noreply.github.com>
2020-04-01 00:58:38 +0300

    Update DetailActivity.java

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,10 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +18,20 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private TextView mAKA_tv, mPlaceOfOrigin_tv, mIngredients_tv, mDescription_tv;
+
+    private String strAka, strPlaceOfOrigin, strIngredient, strDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mAKA_tv = findViewById(R.id.also_known_tv);
+        mPlaceOfOrigin_tv = findViewById(R.id.origin_tv);
+        mIngredients_tv = findViewById(R.id.ingredients_tv);
+        mDescription_tv = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -39,16 +50,21 @@ public class DetailActivity extends AppCompatActivity {
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
+
             closeOnError();
             return;
         }
+        strAka = sandwich.getAlsoKnownAs().toString();
+        strDescription = sandwich.getDescription();
+        strIngredient = sandwich.getIngredients().toString();
+        strPlaceOfOrigin = sandwich.getPlaceOfOrigin();
 
-        populateUI();
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
+        populateUI();
     }
 
     private void closeOnError() {
@@ -57,6 +73,12 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+
+        mAKA_tv.setText(strAka.subSequence(1,strAka.length()-1));
+        mIngredients_tv.setText(strIngredient.subSequence(1,strIngredient.length()-1));
+        mPlaceOfOrigin_tv.setText(strPlaceOfOrigin);
+        mDescription_tv.setText(strDescription);
+
 
     }
 }
